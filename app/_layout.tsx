523ce9e2809c07a37430240715c27e53e724f11c
Salman Shaikh <salman.shaikh@propelius.tech>
2024-08-23 17:27:56 +0530

    add root and auth pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,11 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
     "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
@@ -39,6 +29,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Href, Redirect } from "expo-router";
 
 export default function index() {
-  return (
-    <SafeAreaView className="flex-1">
-      <Text>index</Text>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/(auth)/welcome" as Href} />;
 }
