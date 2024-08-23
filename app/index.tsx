fae64343b09b72bfefe3deae62c7a3bb437dbdff
Salman Shaikh <salman.shaikh@propelius.tech>
2024-08-23 17:03:41 +0530

    added assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function index() {
   return (
-    <View>
+    <SafeAreaView className="flex-1">
       <Text>index</Text>
-    </View>
-  )
+    </SafeAreaView>
+  );
 }
